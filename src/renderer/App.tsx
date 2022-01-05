08fcfef8a8f67c2d30eb21d5f9b082b5a476bb49
Suryansh-23 <suryanshchauhan2005@gmail.com>
2022-01-05 22:46:25 +0530

    Files missed during initial commit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,76 @@
-import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import useHash from './useHash';
+import 'primereact/resources/themes/vela-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'file-icons-js/css/style.css';
 import './App.css';
-
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+import Home from './Home';
+import AES from './AES';
+import SideButton from './SideButton';
+import SideBar from './Sidebar';
+import FileVault from './FileVault';
 
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Hello} />
-      </Switch>
-    </Router>
-  );
+    const [showSidebar, setShowSidebar] = useState(false);
+    const [hash] = useHash();
+
+    const pageVariants = {
+        enter: {
+            x: 1000,
+            opacity: 0,
+            scale: 0,
+        },
+        sidebar: {
+            x: 0,
+            opacity: 1,
+            scale: 1,
+        },
+        center: {
+            x: 0,
+            opacity: 1,
+            scale: 1,
+        },
+        exit: {
+            x: -1000,
+            opacity: 0,
+            scale: 0,
+        },
+        transition: {
+            type: 'Inertia',
+            delay: 0.5,
+        },
+    };
+
+    return (
+        <>
+            <SideButton setShowSidebar={setShowSidebar} />
+            <AnimatePresence exitBeforeEnter initial={false}>
+                <HashRouter>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home pageVariants={pageVariants} hash={hash} />
+                        </Route>
+                        <Route path="/aes" exact>
+                            <AES pageVariants={pageVariants} hash={hash} />
+                        </Route>
+                        <Route path="/file-vault" exact>
+                            <FileVault
+                                pageVariants={pageVariants}
+                                hash={hash}
+                            />
+                        </Route>
+                        <Route path="/stegano" />
+                    </Switch>
+                </HashRouter>
+            </AnimatePresence>
+            <SideBar
+                showSidebar={showSidebar}
+                setShowSidebar={setShowSidebar}
+            />
+        </>
+    );
 }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,5 @@
 import { render } from 'react-dom';
 import App from './App';
 
+window.location.hash = '/';
 render(<App />, document.getElementById('root'));
